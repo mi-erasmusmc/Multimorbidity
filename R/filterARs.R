08f7b809863c8412b3fe0267e8fd4c7a5a869539
Solon Ioannou <sioann88@gmail.com>
2025-06-19 01:00:05 +0200

    Adding package functions

---
+++ R/filterARs.R
@@ -0,0 +1,26 @@
+#' @export
+filterARs <- function(rules, transactions, ...){
+  
+  input_list <- list(...)
+  
+  nonredundant <- rules[!is.redundant(rules)]
+  
+  quality(nonredundant) <- quality(nonredundant) %>%
+    mutate(hyperConfidence = interestMeasure(nonredundant, measure = "hyperConfidence", transactions = input$transactions), 
+           stdLift = interestMeasure(nonredundant, "stdLift", transactions = input$transactions), 
+           chisq = interestMeasure(nonredundant, "chiSquared", transactions = input$transactions, significance = TRUE), 
+           adjusted_chisq = p.adjust(chisq, "fdr"),
+           relativeRisk = interestMeasure(nonredundant, measure = "relativeRisk", transactions = input$transactions),
+           size = size(nonredundant))
+  
+  if ((all(names(input_list) %in% c("support", "confidence", "lift", "hyperConfidence", "stdLift", "chisq", "adjusted_chisq", "relativeRisk", "size"))== TRUE) == FALSE){
+    stop("Not all arguments to filter are supported. See documentation for which measures are allowed to filter from.")
+  } else {
+    
+    for (variable in names(input_list)){
+      result <- subset(nonredundant, subset = base::eval(base::parse(text = base::paste(variable, input_list[[variable]]))))
+    }
+  }
+  
+  return(result)
+}
