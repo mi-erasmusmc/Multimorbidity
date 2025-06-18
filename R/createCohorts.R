9bcb56f210b3d7a640eb7233efe753c53bf03b1f
Solon Ioannou <sioann88@gmail.com>
2025-06-19 00:56:14 +0200

    Function to create cohorts

--- R/createCohorts.R
+++ R/createCohorts.R
@@ -1,30 +1,84 @@
 #' @export
-createCohorts <- function(connectionDetails, 
-                          analysisDetails){
+createCohorts <- function(cohorts = NULL,
+                          connectionDetails = NULL,
+                          cdmDatabaseSchema = cdmDatabaseSchema,
+                          cohortDatabaseSchema = cohortDatabaseSchema,
+                          cohortTable = cohortTable,
+                          incremental = FALSE,
+                          rawDataFolder = "rawData",
+                          saveDirectory = getwd()
+                          ){
   
-  cohortTable <- analysisDetails$cohortTable
-  cdmDatabaseSchema <- analysisDetails$cdmDatabaseSchema
-  cohortDatabaseSchema <- analysisDetails$cohortDatabaseSchema
-  incremental <- analysisDetails$incremental
+  if (dirname(rawDataFolder) == "."){
+    outputFolder = file.path(saveDirectory, rawDataFolder)
+  } else {
+    outputFolder = file.path(rawDataFolder)
+  }
+  
   projectRoot <- system.file(package = "Multimorbidity")
   
   
-  cohortDefinitionSet <- CohortGenerator::getCohortDefinitionSet(settingsFileName = file.path(projectRoot, "settings", "CohortsToCreate.csv"), 
-                                                                 jsonFolder = file.path(projectRoot,"cohorts"), 
-                                                                 sqlFolder = file.path(projectRoot, "sql", "sql_server")
+  ParallelLogger::logInfo(paste("Creating target cohort..."))
+  cohortDefinitionSet <- CohortGenerator::getCohortDefinitionSet(settingsFileName = file.path(projectRoot, "targetDefinitions", "settings", "CohortsToCreate.csv"), 
+                                                                 jsonFolder = file.path(projectRoot, "targetDefinitions", "cohorts"), 
+                                                                 sqlFolder = file.path(projectRoot, "targetDefinitions", "sql", "sql_server")
   )
   
   cohortTableNames <- CohortGenerator::getCohortTableNames(cohortTable = cohortTable)
   
   CohortGenerator::createCohortTables(connectionDetails = connectionDetails, 
                                       cohortDatabaseSchema = cohortDatabaseSchema, 
-                                      cohortTableNames = cohortTableNames, incremental = FALSE)
+                                      cohortTableNames = cohortTableNames, 
+                                      incremental = incremental)
   
   CohortGenerator::generateCohortSet(connectionDetails = connectionDetails,
                                      cdmDatabaseSchema = cdmDatabaseSchema, 
                                      cohortDatabaseSchema = cohortDatabaseSchema,
                                      cohortTableNames = cohortTableNames, 
                                      cohortDefinitionSet = cohortDefinitionSet, 
-                                     incremental = FALSE)
-  return(invisible())
+                                     incremental = incremental, 
+                                     incrementalFolder = outputFolder)
+  
+  # cohortCountsTarget <- CohortGenerator::getCohortCounts(
+  #   connectionDetails = connectionDetails,
+  #   cohortDatabaseSchema = cohortDatabaseSchema,
+  #   cohortTable = cohortTableNames$cohortTable
+  # )
+  
+  ParallelLogger::logInfo(paste("Cohorts created."))
+  
+  ParallelLogger::logInfo(paste("Creating phenotype cohorts..."))
+  
+  cohortDefinitionSet <- CohortGenerator::getCohortDefinitionSet(settingsFileName = file.path(projectRoot, "multimorbidityDefinitions", "Ho_et_al", "settings", "CohortsToCreate.csv"), 
+                                                                 jsonFolder = file.path(projectRoot, "multimorbidityDefinitions", "Ho_et_al", "cohorts"), 
+                                                                 sqlFolder = file.path(projectRoot, "multimorbidityDefinitions", "Ho_et_al", "sql", "sql_server"), 
+                                                                 )
+  
+  cohortTableNames <- CohortGenerator::getCohortTableNames(cohortTable = cohortTable)
+  
+  CohortGenerator::createCohortTables(connectionDetails = connectionDetails, 
+                                      cohortDatabaseSchema = cohortDatabaseSchema, 
+                                      cohortTableNames = cohortTableNames, 
+                                      incremental = TRUE)
+  
+  CohortGenerator::generateCohortSet(connectionDetails = connectionDetails,
+                                     cdmDatabaseSchema = cdmDatabaseSchema, 
+                                     cohortDatabaseSchema = cohortDatabaseSchema,
+                                     cohortTableNames = cohortTableNames, 
+                                     cohortDefinitionSet = cohortDefinitionSet, 
+                                     incremental = TRUE, 
+                                     incrementalFolder = outputFolder)
+  
+  cohortCounts <- CohortGenerator::getCohortCounts(
+    connectionDetails = connectionDetails,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTableNames$cohortTable
+  )
+  
+  ParallelLogger::logInfo(paste("Phenotype cohorts created."))
+  ParallelLogger::logInfo(paste(cohortCounts))
+  
+  
+  utils::write.csv(cohortCounts, file.path(outputFolder, paste0("cohortCounts_", gsub("-", "", Sys.Date()), ".csv")))
+  return(dplyr::tibble(cohortCounts))
 }
