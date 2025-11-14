882da710753501a94cf20ad30c8589f89f65b96d
Solon Ioannou <sioann88@gmail.com>
2025-11-14 11:31:12 +0100

    - solving bug in eval(). - binding arules functions

--- R/filterARs.R
+++ R/filterARs.R
@@ -3,22 +3,22 @@ filterARs <- function(rules, transactions, ...){
   
   input_list <- list(...)
   
-  nonredundant <- rules[!is.redundant(rules)]
+  nonredundant <- rules[!arules::is.redundant(rules)]
   
-  quality(nonredundant) <- quality(nonredundant) %>%
-    mutate(hyperConfidence = interestMeasure(nonredundant, measure = "hyperConfidence", transactions = input$transactions), 
-           stdLift = interestMeasure(nonredundant, "stdLift", transactions = input$transactions), 
-           chisq = interestMeasure(nonredundant, "chiSquared", transactions = input$transactions, significance = TRUE), 
+  quality(nonredundant) <- arules::quality(nonredundant) %>%
+    dplyr::mutate(hyperConfidence = arules::interestMeasure(nonredundant, measure = "hyperConfidence", transactions = transactions), 
+           stdLift = arules::interestMeasure(nonredundant, "stdLift", transactions = transactions), 
+           chisq = arules::interestMeasure(nonredundant, "chiSquared", transactions = transactions, significance = TRUE), 
            adjusted_chisq = p.adjust(chisq, "fdr"),
-           relativeRisk = interestMeasure(nonredundant, measure = "relativeRisk", transactions = input$transactions),
-           size = size(nonredundant))
+           relativeRisk = arules::interestMeasure(nonredundant, measure = "relativeRisk", transactions = transactions),
+           size = arules::size(nonredundant))
   
   if ((all(names(input_list) %in% c("support", "confidence", "lift", "hyperConfidence", "stdLift", "chisq", "adjusted_chisq", "relativeRisk", "size"))== TRUE) == FALSE){
     stop("Not all arguments to filter are supported. See documentation for which measures are allowed to filter from.")
   } else {
     
     for (variable in names(input_list)){
-      result <- subset(nonredundant, subset = base::eval(base::parse(text = base::paste(quote(variable), input_list[[variable]]))))
+      result <- arules::subset(nonredundant, subset = base::eval(base::parse(text = base::paste(quote(variable), input_list[[variable]]))))
     }
   }
   
